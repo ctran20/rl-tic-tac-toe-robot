d1ba7995666302f7d9aee24a2bc272f5997510c2
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-04-19 14:02:29 -0400

    Set up basic layout for game

--- bullet.py
+++ bullet.py
@@ -1,17 +1,29 @@
+from struct import pack
 import pybullet as p
 import time
 import pybullet_data
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
+p.setGravity(0,0,-9.81)
+
+# Load Models
 planeId = p.loadURDF("plane.urdf")
+<<<<<<< Updated upstream
 startPos = [0,0,1]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
 boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
+=======
+table_a = p.loadURDF("table/table.urdf",[0,0,0])
+tray_a = p.loadURDF("tray/traybox.urdf",[0.5,0.7,0.65])
+tray_b = p.loadURDF("tray/traybox.urdf",[-0.5,-0.7,0.65])
+arm_b = p.loadURDF("franka_panda/panda.urdf",[-0.6,0,0.65], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+arm_b  = p.loadURDF("franka_panda/panda.urdf",[0.6,0,0.65], p.getQuaternionFromEuler([0,0,3]), useFixedBase=1)
+
+>>>>>>> Stashed changes
 #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
 for i in range (10000):
     p.stepSimulation()
     time.sleep(1./240.)
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(arm)
 print(cubePos,cubeOrn)
 p.disconnect()
