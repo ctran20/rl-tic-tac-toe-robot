fa1daa9afa2f3bd2a0bc3ec0e3163bef17e2ef9b
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-05-03 20:43:55 -0400

    Sample game with both arms working

--- bullet.py
+++ bullet.py
@@ -4,25 +4,33 @@ import pybullet as p
 import time
 import pybullet_data
 
-<<<<<<< Updated upstream
-=======
 from PIL import Image
 import PIL
 import cv2
 import numpy as np
 
 # Variables ----------------------------------------------------------------
-#                  B E1  3 E2  5    H   W  X  X  R  L  
-#startingPos = [-1.5, 1, 0, 0, 0, 1.8,0.8, 0, 0, 1, 1]
+"""
+               B  E1  R1  E2  R2    H    W  X  X  R  L  
+position = [-1.5,  1,  0,  0,  0, 1.8, 0.8, 0, 0, 1, 1]
+
+B - Base
+E1 - Elbow 1
+E2 - Elbow 2
+R1 - Elbow 1 Rotate
+R2 - Elbow 2 Rotate
+H - Hand
+W - Wrist rotate
+R - Right finger
+L - Left finger
+
+"""
 x_cubes = [ [-1.5, 1.66, 0,0, 0, 1.85, 0.8],        # 1st Cube
             [-1.5, 1, 0,-1.2, 0, 2.2, 0.8],         # 2nd Cube
             [-1.49, 0.7, 0,-1.72, 0, 2.47, 0.8],    # 3rd Cube
             [-1.48, 0.55, 0,-2.01, 0, 2.6, 0.8],    # 4th Cube
             [-1.48, 0.4, 0,-2.27, 0, 2.7, 0.8]]     # 5th Cube
 pickedUp     = [-1.5, 0.8, 0,-1.2, 0, 2.5, 0.8]
-base_p1     = [-1.0, 0.8, 0,-1.2, 0, 2, 0.6]
-base_p2     = [-0.5, 0.6, 0,-1.6, 0, 2.2, 0.3]
-base_p3     = [-0, 0.4, 0,-2, 0, 2.4, 0.8]
 mid_base    = [-0.8, 0.6, 0,-1.5, 0, 2.2, 0.3]
 main_base    = [0, 0.3, 0,-2.2, 0, 2.5, 0.8]
 
@@ -32,9 +40,9 @@ boxes = [[0.51, 0, 0,-2.8, 0, 2.8, 1.1],    # Box 1
         [0, 0, 0,-2.95, 0, 3, 0.8],         # Box 4
         [0, 0.5, 0,-2.3, 0, 3, 0.8],        # Box 5
         [0, 1.0, 0,-1.2, 0, 2.25, 0.8],     # Box 6
-        [-0.51, 0, 0,-2.8, 0, 2.8, 0.4],    # Box 7
-        [-0.40, 0.5, 0,-2.2, 0, 2.8, 0.4],  # Box 8
-        [-0.28, 1.0, 0,-1.0, 0, 2.0, 0.6]]  # Box 9
+        [-0.51, 0, 0,-2.7, 0, 2.8, 0.3],    # Box 7
+        [-0.36, 0.5, 0,-2.1, 0, 2.8, 0.4],  # Box 8
+        [-0.20, 1.0, 0,-0.9, 0, 2.0, 0.5]]  # Box 9
 
 quick = 60
 slow = 90
@@ -44,28 +52,24 @@ o_picked = 0
 # Functions ----------------------------------------------------------------
 
 # Step simulation
->>>>>>> Stashed changes
 def stepSim(steps):
     for _ in range(steps):
         p.stepSimulation()
         time.sleep(0.01)
 
 def grab(arm):
-    p.setJointMotorControl2(arm, 10, p.POSITION_CONTROL,targetPosition=0.02, force=50)
-    p.setJointMotorControl2(arm, 9, p.POSITION_CONTROL,targetPosition=0.02, force=50)
+    p.setJointMotorControl2(arm, 10, p.POSITION_CONTROL,targetPosition=0.0185, force=60)
+    p.setJointMotorControl2(arm, 9, p.POSITION_CONTROL,targetPosition=0.0185, force=60)
+    stepSim(quick)
 
 def release(arm):
     p.setJointMotorControl2(arm, 10, p.POSITION_CONTROL,targetPosition=1.5)
     p.setJointMotorControl2(arm, 9, p.POSITION_CONTROL,targetPosition=1.5)
+    stepSim(quick)
 
 def resetPos(arm):
-    startingPos = [-1.5, 1, 0, 0, 0, 1.8,0.8, 0, 0, 1, 1]
+    startingPos = [-1.5, 1, 0, -0.8, 0, 1.8,0.8, 0, 0, 1, 1]
     p.setJointMotorControlArray(arm, range(11), p.POSITION_CONTROL,targetPositions=startingPos)
-<<<<<<< Updated upstream
-
-def move_arm(arm, pos):
-    p.setJointMotorControlArray(arm, range(7), p.POSITION_CONTROL,targetPositions=pos)
-=======
     stepSim(quick)
 
 def move_arm(arm, pos, speed):
@@ -82,7 +86,7 @@ def load_cubes():
 
     for i in range(5):
         x_cube[i] = p.loadURDF("cube_small.urdf",[-0.49,-0.54 - incr,0.73])
-        o_cube[i] = p.loadURDF("cube_small.urdf",[0.6,0.49 + incr,0.73])
+        o_cube[i] = p.loadURDF("cube_small.urdf",[0.59,0.465 + incr,0.73])
         incr += 0.07
         p.changeVisualShape(o_cube[i], -1, textureUniqueId=o_text)
         p.changeVisualShape(x_cube[i], -1, textureUniqueId=x_text)
@@ -92,6 +96,7 @@ def pick_up_cube(sign):
     global x_picked
     global o_picked
 
+    # Sign 0 for X and 1 for O
     if sign == 0:
         move_arm(arm_a, x_cubes[x_picked%5], quick)
         grab(arm_a)
@@ -103,15 +108,25 @@ def pick_up_cube(sign):
         move_arm(arm_b, pickedUp, slow)
         o_picked += 1
 
-# Place cubes into selected box
+"""
+Place cubes into selected box
+    sign - 0 for X and 1 for O
+    box_num - position to place cube in
+                    O
+            1 2 3 
+            4 5 6
+            7 8 9
+        X
+"""
 def place_cube(sign, box_num):
-    pick_up_cube(sign)
-
+    # Change arm based on sign
     if sign == 0:
         arm = arm_a
     else:
         arm = arm_b
-
+        box_num = 10-box_num
+        
+    pick_up_cube(sign)
     move_arm(arm, mid_base,slow)
     move_arm(arm, main_base, slow)
     move_arm(arm, boxes[box_num-1], slow)
@@ -120,124 +135,59 @@ def place_cube(sign, box_num):
     resetPos(arm)
 
 # Main ---------------------------------------------------------------------
->>>>>>> Stashed changes
 
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-9.81)
+load_cubes()
 
 # Load Models [x, y ,z]
 planeId = p.loadURDF("plane.urdf")
 #p.loadURDF("cube_small.urdf",[-0.49,-0.76,0.72])
-o_cube = p.loadURDF("cube_small.urdf",[-0.49,-0.76,0.73])
-x_cube = p.loadURDF("cube_small.urdf",[-0.49,-0.90,0.73])
-x_cube = p.loadURDF("cube_small.urdf",[-0.49,-0.83,0.73])
-x_cube = p.loadURDF("cube_small.urdf",[-0.49,-0.69,0.73])
-x_cube = p.loadURDF("cube_small.urdf",[-0.49,-0.62,0.73])
-o_text = p.loadTexture("play_cubes/o_cube.png")
-x_text = p.loadTexture("play_cubes/x_cube.png")
-
-p.changeVisualShape(o_cube, -1, textureUniqueId=o_text)
-p.changeVisualShape(x_cube, -1, textureUniqueId=x_text)
 
 table_a = p.loadURDF("table/table.urdf",[0,0,0])
-<<<<<<< Updated upstream
-table_b = p.loadURDF("tic_tac_toe_board/table_square.urdf",[0,0,0])
-tray_a = p.loadURDF("tray/traybox.urdf",[0.5,0.75,0.69])
-tray_b = p.loadURDF("tray/traybox.urdf",[-0.5,-0.75,0.69])
-=======
 table_b = p.loadURDF("tic_tac_toe_board/table_square.urdf",[0,0.005,0])
-tray_a = p.loadURDF("tray/traybox.urdf",[0.5,0.65,0.68])
-tray_b = p.loadURDF("tray/traybox.urdf",[-0.6,-0.68,0.68])
->>>>>>> Stashed changes
+tray_a = p.loadURDF("tray/traybox.urdf",[0.62,0.65,0.67])
+tray_b = p.loadURDF("tray/traybox.urdf",[-0.62,-0.68,0.67])
 arm_a = p.loadURDF("franka_panda/panda.urdf",[-0.55,0,0.65], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
-arm_b  = p.loadURDF("franka_panda/panda.urdf",[0.55,-0.05,0.65], p.getQuaternionFromEuler([0,0,3]), useFixedBase=1)
+arm_b  = p.loadURDF("franka_panda/panda.urdf",[0.548,-0.07,0.65], p.getQuaternionFromEuler([0,0,3]), useFixedBase=1)
 
-joint_nums = p.getNumJoints(arm_a)
-# position, orientation = p.getBasePositionAndOrientation(arm_a)
-# p.getJointInfo(arm_a, 7)
-# E2 ~ H
-#                  B E1  3 E2  5    H   W  X  X  R  L  
-#startingPos = [-1.5, 1, 0, 0, 0, 1.8,0.8, 0, 0, 1, 1]
-
-box_6 = [0, 1.1, 0,-1.15, 0, 2.25, 0.8]
-# pere = p.calculateInverseKinematics(arm_a, 7, [0.1,0.1,0.4])
-trayPrep    = [-1.5, 1, 0,-1.2, 0, 2.2, 0.8]
-pickedUp     = [-1.5, 1, 0,-0.8, 0, 1.8, 0.8]
-base_p1     = [-1.0, 1, 0,-0.8, 0, 1.8, 0.8]
-base_p2     = [-0.5, 1, 0,-0.8, 0, 1.8, 0.8]
-main_base    = [0, 1, 0,-0.8, 0, 1.8, 0.8]
-
-quick = 60
-slow = 100
 p.setRealTimeSimulation(0)
-# Calculate and move arm up after pick up
-#p.setJointMotorControl2(arm_a, 1, p.POSITION_CONTROL, targetPosition=0.5)
 
-<<<<<<< Updated upstream
-p.setJointMotorControlArray(arm_b, range(joint_nums), p.POSITION_CONTROL,
-targetPositions=[1]*joint_nums)
-p.setJointMotorControl2(arm_b, 3, p.POSITION_CONTROL, targetPosition=-2)
-
-resetPos(arm_a)
-stepSim(quick)
-move_arm(arm_a, trayPrep)
-=======
 viewMatrix = p.computeViewMatrix(
     cameraEyePosition=[0, 0, 1.4],
     cameraTargetPosition=[0, 0, 0],
     cameraUpVector=[0, 1, 0])
->>>>>>> Stashed changes
 
-stepSim(quick)
+projectionMatrix = p.computeProjectionMatrixFOV(
+    fov=45.0,
+    aspect=1.0,
+    nearVal=0.1,
+    farVal=3.1)
 
-grab(arm_a)
-stepSim(quick)
 
-<<<<<<< Updated upstream
-move_arm(arm_a, pickedUp)
-stepSim(slow)
-
-move_arm(arm_a, base_p1)
-stepSim(slow)
-=======
 # Controlling Arm ----------------------------------------------------------
 resetPos(arm_a)
 resetPos(arm_b)
 
-place_cube(0,6)
 place_cube(0,5)
-place_cube(0,7)
+place_cube(1,2)
+place_cube(0,1)
+place_cube(1,9)
+place_cube(0,4)
+place_cube(1,7)
+place_cube(0,6)
 
-place_cube(1,1)
-place_cube(1,4)
-place_cube(1,6)
+# --------------------------------------------------------------------------
 
-place_cube(0,2)
->>>>>>> Stashed changes
+img = p.getCameraImage(300,300,viewMatrix=viewMatrix,projectionMatrix=projectionMatrix,renderer=p.ER_TINY_RENDERER)
 
-move_arm(arm_a, base_p2)
-stepSim(slow)
-
-move_arm(arm_a, main_base)
-stepSim(slow)
-
-move_arm(arm_a, box_6)
-stepSim(slow)
-
-release(arm_a)
-stepSim(quick)
-
-move_arm(arm_a, main_base)
-stepSim(quick)
-
-resetPos(arm_a)
-stepSim(quick)
-
-p.getCameraImage(400,400,renderer=p.ER_TINY_RENDERER)
+rgbBuf = img[2]
+rgbim = Image.fromarray(rgbBuf)
 
 cubePos, cubeOrn = p.getBasePositionAndOrientation(arm_a)
 print(cubePos,cubeOrn)
+print("\nFinished!\n")
 
 while True:
     p.stepSimulation()
